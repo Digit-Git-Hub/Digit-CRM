20225736bde08507e4c49c03de874d247c7c0beb
Digit <gvieira@digit.com.pt>
2026-07-26 16:40:21 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const C='digit-crm-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||!e.request.url.startsWith(self.location.origin))return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request)));
+});
